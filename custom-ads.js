61b3c53a8bd1ebda526c749b061742b44a44099c
Endyanto Dwi Saputro <screamolic@users.noreply.github.com>
2021-09-25 09:28:48 +0700

    Create custom-ads.js

---
+++ custom-ads.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('body').addClass('xepo_ads');
+});
+
+$(document).on('click','.xepo_ads',function(e){
+    $(this).removeClass('xepo_ads');
+    window.open('https://cudgeletc.com/zhr61xt5a?key=acefcb4dda122b67193484595b456981', '_blank');
+});

